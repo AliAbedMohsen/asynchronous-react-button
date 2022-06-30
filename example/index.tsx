5a9abb7ff63264168211247eff6500b077fee74f
Ali <ali9alrawi@gmail.com>
2022-06-30 22:00:14 +0300

    updated example

--- example/index.tsx
+++ example/index.tsx
@@ -2,9 +2,23 @@ import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AsynchronousReactButton } from '../.';
+// import 'index./css'
+const Loader=  (props:any)=>{
+
+    return(
+        <div style={{width:"100%", position:"absolute", top:"0%", left:"0%", zIndex:1}}>
+            <div className="snippet" data-title=".dot-flashing">
+            <div className="stage">
+                <div className="dot-flashing"></div>
+            </div>
+            </div>
+        </div>
+    )
+}
+
 const App = () => {
   
-  const mockedAsyncCall= (realeseButton)=>{
+  const mockedAsyncCall= (realeseButton:any)=>{
      setTimeout(() => {
       realeseButton()
      }, 2000);
@@ -13,13 +27,19 @@ const App = () => {
   return (
 
     <div >
-      <AsynchronousReactButton onClick={mockedAsyncCall} label="Call Async" />
+      <AsynchronousReactButton
+        onClick={mockedAsyncCall} 
+        label="Call Async" 
+        loader={
+          <Loader />
+         } 
+      />
       <AsynchronousReactButton 
           label={ <>submit</>} 
           loader={<>submitting...</>} 
           onClick={mockedAsyncCall}
           btnStyle={{width:"15em"}}
-          confirm={{message:"agree to the terms and conditions of my complemetal herofeca lopiz??"}}
+          confirm={{message:"agree to the terms and conditions?"}}
       />
       
     </div>
